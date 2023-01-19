94cd368d5d9aa2ef67ed5b5e80798e28aaa8ea59
SLesche <sven.lesche@itb-consulting.de>
2023-01-19 10:51:33 +0100

    cleaner data, read all of it

---
+++ markdown/r_code/S_brms_testing.R
@@ -0,0 +1,24 @@
+library(brms) # for the analysis
+library(haven) # to load the SPSS .sav file
+library(tidyverse) # needed for data manipulation.
+library(RColorBrewer) # needed for some extra colours in one of the graphs
+library(ggmcmc)
+library(ggthemes)
+library(ggridges)
+
+popular2data <- read_sav(file = "https://github.com/MultiLevelAnalysis/Datasets-third-edition-Multilevel-book/blob/master/chapter%202/popularity/SPSS/popular2.sav?raw=true")
+
+interceptonlymodeltest <- brm(popular ~ 1 + (1 | class), 
+                              data   = popular2data, 
+                              warmup = 100, 
+                              iter   = 200, 
+                              chains = 2, 
+                              inits  = "random",
+                              cores  = 2)  #the cores function tells STAN to make use of 2 CPU cores simultaneously instead of just 1.
+
+interceptonlymodel <- brm(popular ~ 1 + (1|class),  
+                          data = popular2data, 
+                          warmup = 1000, iter = 3000, 
+                          cores = 2, chains = 2, 
+                          seed = 123,
+                          refresh = 10) #to run the model
