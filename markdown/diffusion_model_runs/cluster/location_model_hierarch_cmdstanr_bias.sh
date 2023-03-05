ee069620efffe7386378e53e8750ce9b5b972054
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2023-03-05 20:35:13 +0100

    cluster-push bias

---
+++ markdown/diffusion_model_runs/cluster/location_model_hierarch_cmdstanr_bias.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=location-model-hierarch-bias
+#SBATCH --time=72:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=64
+#SBATCH --mail-type=all
+#SBATCH --mail-user=sven.lesche@stud.uni-heidelberg.de
+
+module load math/R/4.1.2
+module load devel/tbb/2021.4.0
+export TBB_LIB=$TBB_LIB_DIR
+export TBB_INC=$TBB_INC_DIR
+export TBB_INTERFACE_NEW='true'
+Rscript bachelor/location_model_hierarch_cmdstanr_bias.R
