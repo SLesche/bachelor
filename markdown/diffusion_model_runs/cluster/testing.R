f8fc6ffcce8a03b31e353827fa8b730f0f54ea22
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2023-03-07 20:20:01 +0100

    cleanup files

--- markdown/diffusion_model_runs/cluster/testing.R
+++
@@ -1,8 +0,0 @@
-library(brms)
-library(cmdstanr)
-
-fit <- brm(count ~ zAge + zBase * Trt + (1|patient),
-           data = epilepsy, family = negbinomial(),
-           chains = 1, threads = threading(2, grainsize = 100),
-           backend = "cmdstanr")
-
