c67cdc443e9159264696fc1e70da4710d71bdf59
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2023-06-25 21:06:36 +0200

    changes kathrin, conclusion missing

--- psychopy/00_bat_bachelor_lastrun.py
+++ psychopy/00_bat_bachelor_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2022.2.4),
-    on December 17, 2022, at 08:32
+    on April 28, 2023, at 10:28
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -67,7 +67,7 @@ filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expNa
 # An ExperimentHandler isn't essential but helps with data saving
 thisExp = data.ExperimentHandler(name=expName, version='',
     extraInfo=expInfo, runtimeInfo=None,
-    originPath='C:\\Users\\Sven\\Documents\\Psychologie\\Bachelor-Arbeit\\sven_lesche_ba\\psychopy\\00_bat_bachelor_lastrun.py',
+    originPath='C:\\Users\\Sven\\Documents\\Psychologie\\Bachelor-Arbeit\\git\\bachelor\\psychopy\\00_bat_bachelor_lastrun.py',
     savePickle=True, saveWideText=True,
     dataFileName=filename)
 # save a log file for detail verbose info
